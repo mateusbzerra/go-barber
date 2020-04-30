100ff0d7582c69cd515d80f7d13709faac5d9c21
Mateus Bezerra <mateusbzerra@gmail.com>
2020-04-29 23:24:28 -0300

    feat: validate form errors with Yup

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border: 2px solid #232129;
   border-radius: 10px;
@@ -15,6 +21,26 @@ export const Container = styled.div`
     margin-top: 8px;
   }
 
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+   
+
   input {
     flex: 1;
     background: transparent;
@@ -26,5 +52,12 @@ export const Container = styled.div`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+export const Error = styled.div`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0px;
   }
 `;
