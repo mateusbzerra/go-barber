e54dc9c68c8f532efffd9e1da424442817a41121
Mateus Bezerra <mateusbzerra@gmail.com>
2020-05-30 15:54:28 -0300

    tests: add create a new appointment

---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  test('It should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+  // test('It should not be able to create two appointments at the same datetime', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
