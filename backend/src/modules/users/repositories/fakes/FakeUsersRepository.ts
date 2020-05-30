ea39d75df377f3b477761e9cda175701a7fb317c
Mateus Bezerra <mateusbzerra@gmail.com>
2020-05-30 19:17:07 -0300

    test(backend): add CreateUserService tests

---
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,40 @@
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import { uuid } from 'uuidv4';
+import User from '../../infra/typeorm/entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.ormRepository.find(user => user.id === id);
+    return findUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const findUser = this.ormRepository.find(user => user.email === email);
+    return findUser;
+  }
+
+  public async create({
+    name,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid(), name, email, password });
+
+    this.ormRepository.push(user);
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.ormRepository.findIndex(
+      findUser => findUser.id === user.id,
+    );
+    this.ormRepository[findIndex] = user;
+    return user;
+  }
+}
+export default UsersRepository;
---
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,46 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+
+describe('CreateUser', () => {
+  test('It should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const user = await createUserService.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+    expect(user).toHaveProperty('id');
+  });
+
+  test('It should not be able to create a user with an exisisting email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const userEmail = 'foo@bar.com';
+    await createUserService.execute({
+      name: 'Foo Bar',
+      email: userEmail,
+      password: '123456',
+    });
+
+    expect(
+      createUserService.execute({
+        name: 'Foo Bar Two',
+        email: userEmail,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
