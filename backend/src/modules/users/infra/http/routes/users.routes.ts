a2517c3183eae485c431e08001c21b5e23d77cbd
Mateus Bezerra <mateusbzerra@gmail.com>
2020-06-23 21:41:59 -0300

    feat(backend): add users routes validation

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Segments, Joi } from 'celebrate';
 import UserAvatarController from '../controllers/UserAvatarController';
 
 import UsersController from '../controllers/UsersController';
@@ -11,10 +12,21 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
+
   AuthMiddleware,
   upload.single('avatar'),
   userAvatarController.update,
