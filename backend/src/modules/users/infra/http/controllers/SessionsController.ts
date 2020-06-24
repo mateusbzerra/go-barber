365fb62d120ba6d01e68277cebc49e711f9d4d39
Mateus Bezerra <mateusbzerra@gmail.com>
2020-06-23 22:11:57 -0300

    feat(backend): applies class-transformer to User entity & controllers

--- backend/src/modules/users/infra/http/controllers/SessionsController.ts
+++ backend/src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import LoginUserService from '@modules/users/services/LoginUserService';
+import { classToClass } from 'class-transformer';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
@@ -10,7 +11,7 @@ export default class SessionsController {
       email,
       password,
     });
-    delete user.password;
-    return response.json({ user, token });
+
+    return response.json({ user: classToClass(user), token });
   }
 }
