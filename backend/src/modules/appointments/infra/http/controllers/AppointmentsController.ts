b6954473084c16c0eed09bd8398afd39232fbbb3
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-04 01:02:40 -0300

    refactor(backend):remove formatedDate from AppointmentsController because of Joi validation

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,10 +7,10 @@ export default class AppointmentController {
   public async create(request: Request, response: Response): Promise<Response> {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
-    const formattedDate = parseISO(date);
+
     const createAppointment = container.resolve(CreateAppointmentService);
     const appointment = await createAppointment.execute({
-      date: formattedDate,
+      date,
       provider_id,
       user_id,
     });
