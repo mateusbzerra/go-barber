b3a25db6fb469f39419bc41a84a5d3a476c8ed57
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-11 18:00:12 -0300

    fix(backend): list appointments after the current date

--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,6 @@
 // import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 // import User from '@modules/users/infra/typeorm/entities/User';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -34,12 +34,16 @@ export default class ListProviderMonthAvailabilityService {
       (_, index) => index + 1,
     );
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day;
       });
+
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
     return availability;
