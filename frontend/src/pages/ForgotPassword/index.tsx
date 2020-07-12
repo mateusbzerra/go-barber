fa1a3ab4ce4815e09507cd84ea19fe2074df0ec9
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-11 21:30:25 -0300

    feat(frontend): add ForgotPassword page

---
+++ frontend/src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,84 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+
+import { useToast } from '../../hooks/toast';
+import { Container, Content, AnimationContainer, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+
+interface ForgotPasswordFormData {
+  email: string;
+  password: string;
+}
+
+const ForgotPassword: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { addToast } = useToast();
+
+  const handleSubmit = useCallback(
+    async (data: ForgotPasswordFormData) => {
+      formRef.current?.setErrors({});
+      try {
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Digite um e-mail válido')
+            .required('E-mail obrigatório'),
+        });
+
+        await schema.validate(data, { abortEarly: false });
+        await api.post('/password/forgot', {
+          email: data.email,
+        });
+
+        addToast({
+          type: 'success',
+          title: 'Email de recuperação enviado',
+          description:
+            'Enviamos um email para confirmar a recuperação de senha',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na recuperação de senha',
+          description:
+            'Ocorreu um erro ao tentar realizar a recuperação de senha',
+        });
+      }
+    },
+    [addToast],
+  );
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="gobarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Recuperar Senha</h1>
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+            <Button type="submit">Recuperar</Button>
+          </Form>
+          <Link to="/signup">
+            <FiLogIn /> Voltar ao login
+          </Link>
+        </AnimationContainer>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default ForgotPassword;
