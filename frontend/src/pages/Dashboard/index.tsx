b4466e91d36cd5fa2f2de21c0cfc5352b10a639d
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-13 20:23:46 -0300

    style(frontend): add appointments list and month calendar

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React, { useCallback, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
   Container,
@@ -9,12 +10,20 @@ import {
   Content,
   Schedule,
   NextAppointments,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
   const { signOut } = useAuth();
   return (
     <Container>
@@ -58,9 +67,98 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointments>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/17644982?s=460&u=eb3394a14934d0228207c4071cc79edf80cac825&v=4"
+                  alt="Mateus Bezerra"
+                />
+                <strong>Mateus Bezerra</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/17644982?s=460&u=eb3394a14934d0228207c4071cc79edf80cac825&v=4"
+                  alt="Mateus Bezerra"
+                />
+                <strong>Mateus Bezerra</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/17644982?s=460&u=eb3394a14934d0228207c4071cc79edf80cac825&v=4"
+                  alt="Mateus Bezerra"
+                />
+                <strong>Mateus Bezerra</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/17644982?s=460&u=eb3394a14934d0228207c4071cc79edf80cac825&v=4"
+                  alt="Mateus Bezerra"
+                />
+                <strong>Mateus Bezerra</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
 
-        <Calendar />
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            selectedDays={selectedDate}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            onDayClick={handleDateChange}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
